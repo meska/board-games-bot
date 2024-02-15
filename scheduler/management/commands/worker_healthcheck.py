0d2c7c65e8eb85f4847570766a586428295d6cae
meska <meskatech@gmail.com>
2024-02-15 16:35:57 +0100

    testing healthcheck

--- scheduler/management/commands/worker_healthcheck.py
+++ scheduler/management/commands/worker_healthcheck.py
@@ -24,22 +24,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         queue = options["queue"]
         from django.core.cache import cache
-
         hb = cache.get(f"healthcheck_{queue}")
-        if timezone.now() - hb > timedelta(minutes=60):
-            print(f"Healthcheck {queue} is too old: {hb}")
-            # invio messaggio a sentry
-            from sentry_sdk import capture_message
-            from sentry_sdk import push_scope
+        if hb:
+            if timezone.now() - hb > timedelta(minutes=60):
+                print(f"Healthcheck {queue} is too old: {hb}")
+                # invio messaggio a sentry
+                from sentry_sdk import capture_message
+                from sentry_sdk import push_scope
 
-            with push_scope() as scope:
-                scope.set_extra("queue", queue)
-                scope.set_extra("last_hearthbeat", hb)
-                scope.set_tag("queue", queue)
-                capture_message(
-                    f"Healthcheck Failed {queue}", level="warning", scope=scope
-                )
-            exit(1)
+                with push_scope() as scope:
+                    scope.set_extra("queue", queue)
+                    scope.set_extra("last_hearthbeat", hb)
+                    scope.set_tag("queue", queue)
+                    capture_message(
+                        f"Healthcheck Failed {queue}", level="warning", scope=scope
+                    )
+                exit(1)
+            else:
+                print(f"Healthcheck {queue} is ok: {hb}")
+                exit(0)
         else:
-            print(f"Healthcheck {queue} is ok: {hb}")
-            exit(0)
+            print(f"Healthcheck {queue} not found")
+            exit(1)
